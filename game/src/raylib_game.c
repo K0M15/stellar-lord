cde9882e13b673b379eef8da2170522cb4e1523d
Alain Felger <alain.felger@googlemail.com>
2024-08-31 21:10:12 +0200

    Added multiple cycle Button.

--- game/src/raylib_game.c
+++ game/src/raylib_game.c
@@ -10,11 +10,25 @@ void action_cycle()
     cycle_game(&game);
 }
 
+void action_cylce_mult()
+{
+    int i = 10;
+    while (--i)
+    {
+        cycle_game(&game);
+    }
+}
+
 t_game *get_game()
 {
     return &game;
 }
 
+
+// Create Logos for the buttons
+// Implement ZOOM
+// Implement AI
+// make an overlay of how many ships to send
 int main(void)
 {
     InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT+100, GAME_NAME);
@@ -26,12 +40,13 @@ int main(void)
         fprintf(stderr, "Error initalizing the game");
     }
     t_player* player = game_request_player(&game, "Alain");
+    t_player* player2 = game_request_player(&game, "Steffen");
     t_position uipos;
     uipos.x = 0;
     uipos.y = SCREEN_HEIGHT;
     ui_button_create(&game.ui, 'N', uipos, action_cycle);
-    uipos.x = 100;
-    ui_button_create(&game.ui, 'C', uipos, action_cycle);
+    uipos.x += 100;
+    ui_button_create(&game.ui, 'M', uipos, action_cylce_mult);
     // CREATE FIGHTS and OVERTAKES
     while (!WindowShouldClose())
     {
@@ -43,10 +58,11 @@ int main(void)
                 game.selected_star->position.x,
                 game.selected_star->position.y,
                 100,
-                RED);
+                game.selected_star->owner ? ColorBrightness(game.selected_star->owner->color, -.13) : RAYWHITE);
         for (int i = game.stars_filled; i; --i)
         {
-            draw_star(&game.stars[i-1]);
+            Vector2 vec = GetMousePosition();
+            draw_star(&game.stars[i-1], vec);
         }
         t_cluster_list *list = game.clusters;
         while (list != NULL)
